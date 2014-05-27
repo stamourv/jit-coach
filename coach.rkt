fd3332e59bb90d11a8f63cc933051e8a18e8748a
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-27 10:57:21 -0700

    Another failure explanation.

--- coach.rkt
+++ coach.rkt
@@ -384,6 +384,7 @@
 ;; done on a case-by-case basis, work in progress.
 (define (explain-failure failure event)
   (match (failure-reason failure)
+
     ["would require a barrier"
      (unless (equal? "setprop" (optimization-event-operation event))
        (error "should only happen for getprop"))
@@ -398,6 +399,13 @@
               unexpected-type)
       "which causes a type barrier to be added, which disables some "
       "optimizations\n\n")]
+
+    ["property not in a fixed slot"
+     (string-append
+      "this property is not guaranteed to always be in the same location\n"
+      "are you initializing it in multiple locations?\n"
+      "if so, try initializing it in only one\n\n")]
+
     [reason ;; TODO implement more
      (format "~a (no explanation implemented yet!)\n\n" reason)]))
 
