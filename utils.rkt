30e7f53cde6704c652a7b2f57b6a82faa4895e37
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-28 11:10:12 -0700

    Move by-location reporting to its own file.

--- utils.rkt
+++ utils.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang racket
 
 ;; misc. functions
 
@@ -6,3 +6,9 @@
 
 (define separator (make-string 80 #\-))
 (define (print-separator) (displayln separator))
+
+(define (all-the-same? l)
+  (cond [(empty? l) #t]
+        [else
+         (define head (first l))
+         (andmap (lambda (x) (equal? x head)) (rest l))]))
