6b18c6ff009d756220d20759dc2a58de0a7fdc45
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-28 10:43:25 -0700

    Move structs and helpers to their own file.

--- coach.rkt
+++ coach.rkt
@@ -2,6 +2,8 @@
 
 (require unstable/list)
 
+(require "structs.rkt")
+
 (define separator (make-string 80 #\-))
 (define (print-separator) (displayln separator))
 
@@ -30,33 +32,6 @@
                  rev-events)))
 
 ;; second, parse location (and maybe some general info)
-;; location : location?
-;; operation : "getprop" | "setprop"
-;; type-dict : (dictof string? string?)
-;;   mapping "operand name" to their possible types
-;; attempts : (listof attempt?)
-(struct optimization-event (location operation property type-dict attempts)
-        #:transparent
-        #:mutable) ; so attempts can refer back to the event
-;; file + line + column are not enough to disambiguate. script + offset is
-;; also includes operation + property, for printing
-(struct location (file line column script offset operation property)
-        #:transparent
-        #:methods gen:custom-write
-        [(define (write-proc location port _)
-           (fprintf port "~a:~a:~a (~a ~a, offset: ~a)"
-                    (location-file location)
-                    (location-line location)
-                    (location-column location)
-                    (location-operation location)
-                    (location-property location)
-                    ;; because line+column info seems to point at the
-                    ;; *statement*, there may be multiple, e.g., getprop x
-                    ;; at the same location (e.g. v.x + w.x).
-                    ;; bytecode offset allows us to disambiguate, but
-                    ;; doesn't really point precisely, which is bad.
-                    ;; TODO is there a way to get more precise info?
-                    (location-offset location)))])
 
 ;; parse-event : (listof string?) -> optimization-event?
 (define (parse-event e)
@@ -119,20 +94,6 @@
   (set-optimization-event-attempts! event (parse-attempts attempts-log event))
   event)
 
-
-;; strategy : string?
-;; event : optimization-event? ; the event during which this attempt was made
-;; TODO grammar is a bit inconsistent in logs. sometimes lists the operation
-;;   too, sometimes is a verb phrase, sometimes a noun. clean up
-(struct attempt (strategy event) #:transparent)
-;; reason : string?
-(struct failure attempt (reason) #:transparent)
-;; details: string? ; e.g. what sub-strategy succeeded
-(struct success attempt (details) #:transparent)
-;; TODO maybe not have sub-strategies, and consider each as a top-level
-;;  success/failure. currently, e.g., inlining a poly getprop is a success,
-;;  but it's less good than inlining a mono one. but that doesn't show up as
-;;  a failure
 
 ;; given a list of lines that describe attempts at different implementation
 ;; strategies for an operation, parse what succeeded, what failed, and why
@@ -256,14 +217,6 @@
 
 ;; TODO try a simpler one, that just checks for monotonicity (never see an old one again)
 
-
-(define (event-failures event)
-  (filter failure? (optimization-event-attempts event)))
-(define (event-strategy event)
-  (for/first ([a (optimization-event-attempts event)]
-              #:when (success? a))
-    (match-define (success strategy event details) a)
-    (if details (format "~a (~a)" strategy details) strategy)))
 
 ;; from-event, to-event : optimization-event? ; before and after regression
 ;; got-better? : (or/c optimization-event? #f) ; event at which we got better
@@ -415,11 +368,6 @@
 ;; where failure to see that type was the problem (e.g. non-executed
 ;; code, that would have seen that type, but didn't so didn't know how
 ;; to optimize).
-
-(define (event-object-type event)
-  (dict-ref (optimization-event-type-dict event) "obj"))
-(define (single-object-type? event)
-  (regexp-match "^object\\[1\\]" (event-object-type event)))
 
 ;; group-by-object-type : (listof optimization-event?)
 ;;                          -> (listof (listof optimization-event?)
---
+++ structs.rkt
@@ -0,0 +1,66 @@
+#lang racket
+
+;;;; struct definitions and struct-specific helpers
+
+(provide (all-defined-out))
+
+;; location : location?
+;; operation : "getprop" | "setprop"
+;; type-dict : (dictof string? string?)
+;;   mapping "operand name" to their possible types
+;; attempts : (listof attempt?)
+(struct optimization-event (location operation property type-dict attempts)
+        #:transparent
+        #:mutable) ; so attempts can refer back to the event
+
+(define (event-object-type event)
+  (dict-ref (optimization-event-type-dict event) "obj"))
+(define (single-object-type? event)
+  (regexp-match "^object\\[1\\]" (event-object-type event)))
+
+
+;; file + line + column are not enough to disambiguate. script + offset is
+;; also includes operation + property, for printing
+(struct location (file line column script offset operation property)
+        #:transparent
+        #:methods gen:custom-write
+        [(define (write-proc location port _)
+           (fprintf port "~a:~a:~a (~a ~a, offset: ~a)"
+                    (location-file location)
+                    (location-line location)
+                    (location-column location)
+                    (location-operation location)
+                    (location-property location)
+                    ;; because line+column info seems to point at the
+                    ;; *statement*, there may be multiple, e.g., getprop x
+                    ;; at the same location (e.g. v.x + w.x).
+                    ;; bytecode offset allows us to disambiguate, but
+                    ;; doesn't really point precisely, which is bad.
+                    ;; TODO is there a way to get more precise info?
+                    (location-offset location)))])
+
+
+;; strategy : string?
+;; event : optimization-event? ; the event during which this attempt was made
+(struct attempt (strategy event) #:transparent)
+
+;; reason : string?
+(struct failure attempt (reason) #:transparent)
+
+;; details: string? ; e.g. what sub-strategy succeeded
+(struct success attempt (details) #:transparent)
+;; TODO maybe not have sub-strategies, and consider each as a top-level
+;;  success/failure. currently, e.g., inlining a poly getprop is a success,
+;;  but it's less good than inlining a mono one. but that doesn't show up as
+;;  a failure
+
+
+(define (event-failures event)
+  (filter failure? (optimization-event-attempts event)))
+
+;; strategy that was ultimately picked
+(define (event-strategy event)
+  (for/first ([a (optimization-event-attempts event)]
+              #:when (success? a))
+    (match-define (success strategy event details) a)
+    (if details (format "~a (~a)" strategy details) strategy)))
