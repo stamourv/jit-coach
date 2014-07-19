85b9027d24b917c479a84dda5f1830d5cd7d1a8d
Vincent St-Amour <stamourv@racket-lang.org>
2014-07-18 18:55:03 -0700

    Remove dead code.

--- utils.rkt
+++ utils.rkt
@@ -6,9 +6,3 @@
 
 (define separator (make-string 80 #\-))
 (define (print-separator) (displayln separator))
-
-(define (all-the-same? l)
-  (cond [(empty? l) #t]
-        [else
-         (define head (first l))
-         (andmap (lambda (x) (equal? x head)) (rest l))]))
